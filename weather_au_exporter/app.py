73b727fbb3f95de8e3131e7762e3dabe4b2132e0
Sean Johnson <seanson@users.noreply.github.com>
2022-06-07 11:09:25 +1000

    feat: Add humidity support

--- weather_au_exporter/app.py
+++ weather_au_exporter/app.py
@@ -9,10 +9,12 @@ from prometheus_client import Gauge
 from prometheus_flask_exporter import PrometheusMetrics
 from pyowm import OWM
 
+
 class Config:
     SCHEDULER_API_ENABLED = True
     LOCATION = os.environ.get("LOCATION", "")
     OWM_API_KEY = os.environ.get("OWM_API_KEY", "")
+
 
 if Config.LOCATION == "":
     print("LOCATION must be set!")
@@ -30,18 +32,32 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 metrics = PrometheusMetrics(app)
-temp_gauge = Gauge("weather_temperature_celcius", "The external temperature reported by the BOM API", labelnames=["location"])
+temp_gauge = Gauge(
+    "weather_temperature_celcius",
+    "The external temperature reported by the BOM API",
+    labelnames=["location"],
+)
+humidity_gauge = Gauge(
+    "weather_humidity_percentage",
+    "The external humidity reported by the BOM API",
+    labelnames=["location"],
+)
+
 
 scheduler = APScheduler()
 scheduler.init_app(app)
 scheduler.start()
 
-@scheduler.task('interval', id='fetch_weather', minutes=30, misfire_grace_time=900)
+
+@scheduler.task("interval", id="fetch_weather", minutes=30, misfire_grace_time=900)
 def fetch_weather():
     logger.info("Fetching weather data for location %s", Config.LOCATION)
     weather_manager
     observation = weather_manager.weather_at_place(Config.LOCATION)
-    temperature = observation.weather.temperature('celsius')['temp']
+    temperature = observation.weather.temperature("celsius")["temp"]
+    humidity = observation.weather.humidity
     temp_gauge.labels(location=Config.LOCATION).set(temperature)
+    humidity_gauge.labels(location=Config.LOCATION).set(humidity)
+
 
 fetch_weather()
